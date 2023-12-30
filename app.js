ad41a9d054538b5d9df636ee18c99b2a76b4a9da
joeld <freakyaye@gmail.com>
2023-12-30 22:24:45 +1100

    changed global variables to session storage, added next() for route which requires multiple endpoints. currently adding track metadata to tracks array.

--- app.js
+++ app.js
@@ -3,32 +3,71 @@ const app = express()
 const axios = require('axios').default
 const crypto = require('crypto')
 const querystring = require("querystring");
+const sqlite = require("better-sqlite3")
+const session = require("express-session")
+const SqliteStore = require("better-sqlite3-session-store")(session)
+const db = new sqlite("sessions.db", {})
 require ('dotenv').config()
 const port = process.env.PORT
 const client_id = process.env.CLIENTID
 const client_secret = process.env.CLIENTSECRET
 const response_type = 'code'
 const redirect_uri = 'http://localhost:3000/callback'
-const state = crypto.randomBytes(16).toString('hex')
 let access_token = null
 const token_type = 'Bearer'
 const accessTokenStartTime = null
 let refresh_token = null
+const secureEnv = function () {
+    if (app.get('env') === 'production') {
+        return true
+    } else {
+        return false
+    }
+}
+
+app.use(session({
+    genid: function () {
+        return crypto.randomUUID()
+    },
+    store: new SqliteStore({
+        client: db,
+        expired: {
+            clear: true,
+            intervalMs: 120 * 60 * 1000 //# Being 2Hrs in ms
+        }
+    }),
+    secret: process.env.SESSIONSECRET,
+    resave: false,
+    cookie: {
+        secure: secureEnv(),
+        path: '/',
+        httpOnly: true,
+        maxAge: 7200000
+    }
+}))
+
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    if (req.session.access_token !== undefined && req.session.access_token !== null) {
+        res.send('Logged In')
+    } else {
+        res.send('Log In')
+    }
 })
 
 app.get('/login', (req, res) => {
     const scope = 'user-top-read'
+
+    const calculateState = crypto.randomBytes(16).toString('hex')
+    req.session.state = calculateState
 
     res.redirect('https://accounts.spotify.com/authorize?' + querystring.stringify({
         client_id: client_id,
         response_type: response_type,
         scope: scope,
         redirect_uri: redirect_uri,
-        state: state
+        state: req.session.state
     }))
 })
 
@@ -38,9 +77,9 @@ app.get('/error', (req, res) => {
 
 app.get('/callback', (req, res) => {
     const code = req.query.code || null
-    const state = req.query.state || null
+    const returnedState = req.query.state || null
 
-    if (state === null) {
+    if (returnedState !== req.session.state) {
         res.redirect('/error?' + querystring.stringify({
             error: 'state_error'
         }))
@@ -59,29 +98,89 @@ app.get('/callback', (req, res) => {
         const response = axios.post('https://accounts.spotify.com/api/token',
             optionsObject,
             axiosConfig).then(function (response) {
-            access_token = response.data.access_token
-            refresh_token = response.data.refresh_token
+            req.session.access_token = response.data.access_token
+            req.session.refresh_token = response.data.refresh_token
+            req.session.save(function (error){
+                if (error !== null) {
+                    console.log(error)
+                }
+            })
+            res.redirect('/')
         }).catch(function (error) {
             console.log(error)
         })
     }
 })
 
-app.get('/myTopTracks', (req, res) => {
+function topTracksApiCall (req, res, next) {
     const response = axios.get('https://api.spotify.com/v1/me/top/tracks', {
-    params: {
-        'time_range': 'medium_term',
-        'limit': '10',
-        'offset': '10'
-    },
-    headers: {
-        'Authorization': 'Bearer ' + access_token
+        params: {
+            'time_range': 'medium_term',
+            'limit': '10',
+            'offset': '10'
+        },
+        headers: {
+            'Authorization': 'Bearer ' + req.session.access_token
         }
-}).then(function (response) {
-    console.log(response.data)
+    }).then(function (response) {
+        req.session.topTracks = response.data.items
+        req.session.save(function (error){
+            if (error !== null) {
+                console.log(error)
+            }
+        })
+        next()
     }).catch(function (error) {
         console.log(error.data)
-    })})
+    })
+
+}
+
+function tracksMetadataApiCall (req, res, next) {
+    const songArray = []
+    req.session.topTracks.forEach((item) => {
+        const songData = {
+            artwork: item.album.images[2].url,
+            albumName: item.album.name,
+            artistName: item.artists[0].name,
+            trackName: item.name,
+            trackId: item.id,
+            popularity: item.popularity,
+            sample: item.preview_url,
+            danceability: null,
+            energy: null,
+            valence: null,
+            tempo: null,
+            uri: null
+        }
+        songArray.push(songData)
+    })
+    req.session.songArray = songArray
+    req.session.save(function (error){
+        if (error !== null) {
+            console.log(error)
+        }
+    })
+    next()
+}
+
+app.get('/mytoptracks', topTracksApiCall, tracksMetadataApiCall, (req, res, next) => {
+    const trackIdsArray = []
+    req.session.songArray.forEach((item) => {
+        trackIdsArray.push(item.trackId)
+    })
+    const response = axios.get('https://api.spotify.com/v1/audio-features', {
+        params: {
+            'ids': trackIdsArray.toString()
+        },
+        headers: {
+            'Authorization': 'Bearer ' + req.session.access_token
+        }
+    }).then(function (response) {
+        console.log(response.data.audio_features)
+    })
+    res.send(req.session.topTrackswithMetaD)
+    })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
