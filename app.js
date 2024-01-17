8ad51399e0440f9c23347c8c4c81a8837ca001f6
joeld <freakyaye@gmail.com>
2024-01-17 16:42:59 +1100

    Added refresh tokens

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const sqlite = require("better-sqlite3")
 const session = require("express-session")
 const stateStore = require('store2')
 const {data, toJSON} = require("express-session/session/cookie");
+const {response} = require("express");
 const SqliteStore = require("better-sqlite3-session-store")(session)
 const db = new sqlite("sessions.db", {})
 require ('dotenv').config()
@@ -19,10 +20,8 @@ const client_secret = process.env.CLIENTSECRET
 const genre = ["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"]
 const response_type = 'code'
 const redirect_uri = 'http://localhost:3000/callback'
-let access_token = null
 const token_type = 'Bearer'
-const accessTokenStartTime = null
-let refresh_token = null
+
 const secureEnv = function () {
     if (app.get('env') === 'production') {
         return true
@@ -49,10 +48,50 @@ app.use(express.json())
 app.use(express.static('public'))
 
 function validAccessToken(req, res, next) {
-    if (req.session.access_token !== null && req.session.access_token !== undefined && req.session.access_token !== '') {
+    if (req.session.access_token !== null && req.session.access_token !== undefined && req.session.access_token !== '' && checkTokenExpiry) {
         next()
     } else {
         res.status(403).send()
+    }
+}
+
+function checkTokenExpiry(req) {
+    const now = new Date()
+    const currentTime = now.getTime()
+    const elapsedTimeMs = Math.abs(req.session.issueTime - currentTime)
+    const elapsedTimeSec = Math.floor(elapsedTimeMs/1000)
+    if (elapsedTimeSec > 3500) {
+        const authString = new Buffer.from(client_id + ':' + client_secret).toString('base64')
+        console.log(req.session.refresh_token)
+        try {
+            axios.post('https://accounts.spotify.com/api/token', {
+                grant_type: 'refresh_token',
+                refresh_token: req.session.refresh_token
+            }, {
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded',
+                    'authorization': 'Basic ' + authString
+                }
+            }).then(response => {
+                if (response.status === 200) {
+                    req.session.access_token = response.data.access_token
+                    req.session.refresh_token = response.data.refresh_token
+                    const date = new Date()
+                    req.session.issueTime = date.getTime()
+                    req.session.save(function (error) {
+                        if (error !== null) {
+                            console.error(error.message)
+                        }
+                    })
+                    return true
+                } else if (response.status !== 200) {
+                    console.error(response)
+                    return false
+                }
+            })
+        } catch (e) {
+            console.log(e)
+        }
     }
 }
 
@@ -77,21 +116,10 @@ app.use(session({
     }
 }))
 
-
-
-// app.get('/', (req, res) => {
-//     if (req.session.access_token !== undefined && req.session.access_token !== null) {
-//         res.send('Logged In')
-//     } else {
-//         res.send('Log In')
-//     }
-// })
-
 app.get('/login', (req, res) => {
     const scope = 'user-top-read playlist-modify-private'
 
     const calculateState = crypto.randomBytes(16).toString('hex')
-    // stateStore('state', calculateState)
     req.session.state = calculateState
 
     req.session.save(function (error){
@@ -138,6 +166,8 @@ app.get('/callback', (req, res) => {
             axiosConfig).then(function (response) {
             req.session.access_token = response.data.access_token
             req.session.refresh_token = response.data.refresh_token
+            const date = new Date()
+            req.session.issueTime = date.getTime()
             req.session.save(function (error){
                 if (error !== null) {
                     console.log(error.data)
@@ -165,7 +195,6 @@ function topTracksApiCall (req, res, next) {
         req.session.save(function (error){
             if (error !== null) {
                 console.log(error.data)
-                console.log('error in save')
             }
         })
 
@@ -186,7 +215,6 @@ function tracksMetadataApiCall (req, res, next) {
             trackName: item.name,
             trackId: item.id,
             popularity: item.popularity,
-            //genre: item.artists[0].genres,
             sample: item.preview_url,
             danceability: null,
             energy: null,
@@ -485,7 +513,6 @@ app.post('/saveplaylist', validAccessToken, (req, res) => {
         }).then(() => {
             const json = JSON.stringify({uris:
                 req.body.songsToAdd})
-            // console.log(json)
             const response = axios.post(`https://api.spotify.com/v1/playlists/${stateStore('newPlaylistID')}/tracks`,
                      json
                 ,
@@ -512,12 +539,6 @@ app.get('/logout', (req, res) => {
     })
     res.status(200).send()
 })
-
-app.get('/testroute', (req, res) => {
-    console.log(stateStore('access_token'))
-    console.log(stateStore('state'))
-    console.log(stateStore('refresh_token'))
-}   )
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
