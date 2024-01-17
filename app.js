8e9b32df663ad324ee503daca189157b4c75e62b
joeld <freakyaye@gmail.com>
2024-01-17 18:21:41 +1100

    updated package json build and run scripts

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ require ('dotenv').config()
 const port = process.env.PORT
 const client_id = process.env.CLIENTID
 const client_secret = process.env.CLIENTSECRET
+const corsValue = process.env.CORSORIGIN
 const genre = ["acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music"]
 const response_type = 'code'
-const redirect_uri = 'http://localhost:3000/callback'
+const redirect_uri = process.env.REDIRECT_URI
 const token_type = 'Bearer'
 
 const secureEnv = function () {
@@ -39,7 +40,7 @@ app.use(helmet({
 }))
 
 app.use(cors({
-    origin: ['http://localhost:3000']
+    origin: [corsValue]
 }))
 
 app.use(express.urlencoded())
